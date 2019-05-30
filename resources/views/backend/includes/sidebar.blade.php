fc38df7cb6786d11cd102f9aed1fb5be3cd039b8
neoilhamakbar <neoilhamakbar123@github.com>
2019-05-30 12:21:34 +0700

    Perbaikan Akhir

--- resources/views/backend/includes/sidebar.blade.php
+++ resources/views/backend/includes/sidebar.blade.php
@@ -5,8 +5,9 @@
                 @lang('menus.backend.sidebar.general')
             </li>
             <li class="nav-item">
-                <a class="nav-link {{ active_class(Active::checkUriPattern('admin/dashboard')) }}"
-                   href="{{ route('admin.dashboard') }}">
+                <a class="nav-link {{
+                    active_class(Active::checkUriPattern('admin/dashboard'))
+                }}" href="{{ route('admin.dashboard') }}">
                     <i class="nav-icon fas fa-tachometer-alt"></i>
                     @lang('menus.backend.sidebar.dashboard')
                 </a>
@@ -16,68 +17,72 @@
                 @lang('menus.backend.sidebar.system')
             </li>
 
-            <li class="nav-item nav-dropdown">
-                <a class="nav-link nav-dropdown-toggle" href="#">
-                    <i class="nav-icon icon-people"></i> Pengelolaan Sivitas</a>
-                <ul class="nav-dropdown-items">
+            @if ($logged_in_user->isAdmin())
+                <li class="nav-item nav-dropdown {{
+                    active_class(Active::checkUriPattern('admin/auth*'), 'open')
+                }}">
+                    <a class="nav-link nav-dropdown-toggle {{
+                        active_class(Active::checkUriPattern('admin/auth*'))
+                    }}" href="#">
+                        <i class="nav-icon far fa-user"></i>
+                        @lang('menus.backend.access.title')
 
-                    {{-- Menu Dosen--}}
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.dosen.index') }}">
-                            <i class="nav-icon fas fa-minus"></i> Dosen
-                        </a>
-                    </li>
+                        @if ($pending_approval > 0)
+                            <span class="badge badge-danger">{{ $pending_approval }}</span>
+                        @endif
+                    </a>
 
-                    {{-- Menu Mahasiswa--}}
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.mahasiswa.index') }}">
-                            <i class="nav-icon fas fa-minus"></i> Mahasiswa
-                        </a>
-                    </li>
+                    <ul class="nav-dropdown-items">
+                        <li class="nav-item">
+                            <a class="nav-link {{
+                                active_class(Active::checkUriPattern('admin/auth/user*'))
+                            }}" href="{{ route('admin.auth.user.index') }}">
+                                @lang('labels.backend.access.users.management')
 
-                </ul>
-            </li>
+                                @if ($pending_approval > 0)
+                                    <span class="badge badge-danger">{{ $pending_approval }}</span>
+                                @endif
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{
+                                active_class(Active::checkUriPattern('admin/auth/role*'))
+                            }}" href="{{ route('admin.auth.role.index') }}">
+                                @lang('labels.backend.access.roles.management')
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            @endif
 
-            <li class="nav-item nav-dropdown">
+            <li class="divider"></li>
 
-                <a class="nav-link nav-dropdown-toggle" href="#">
-                    <i class="nav-icon icon-user-follow"></i> Pengelolaan User</a>
-                <ul class="nav-dropdown-items">
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.permissions.index') }}">
-                            <i class="nav-icon fas fa-minus"></i> Permissions
-                        </a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.roles.index') }}">
-                            <i class="nav-icon fas fa-minus"></i> Roles
-                        </a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.users.index') }}">
-                            <i class="nav-icon fas fa-minus"></i> Users
-                        </a>
-                    </li>
-                </ul>
-            </li>
-
-           <!-- {{--  PENGELOLAAN PENELITIAN--}}
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('admin.penelitian.index') }}">
-                    <i class="nav-icon fas fa-book"></i> Kelola Penelitian
+            <li class="nav-item nav-dropdown {{
+                active_class(Active::checkUriPattern('admin/log-viewer*'), 'open')
+            }}">
+                <a class="nav-link nav-dropdown-toggle {{
+                    active_class(Active::checkUriPattern('admin/log-viewer*'))
+                }}" href="#">
+                    <i class="nav-icon fas fa-list"></i> @lang('menus.backend.log-viewer.main')
                 </a>
-            </li>
-            -->
-            {{--Organisasi Mahasiswa--}}
-            <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.organisasimhs.index') }}">
-                            <i class="nav-icon fas fa-users"></i> Organisasi Mahasiswa
+
+                <ul class="nav-dropdown-items">
+                    <li class="nav-item">
+                        <a class="nav-link {{
+                            active_class(Active::checkUriPattern('admin/log-viewer'))
+                        }}" href="{{ route('log-viewer::dashboard') }}">
+                            @lang('menus.backend.log-viewer.dashboard')
                         </a>
                     </li>
-            
+                    <li class="nav-item">
+                        <a class="nav-link {{
+                            active_class(Active::checkUriPattern('admin/log-viewer/logs*'))
+                        }}" href="{{ route('log-viewer::logs.list') }}">
+                            @lang('menus.backend.log-viewer.logs')
+                        </a>
+                    </li>
+                </ul>
+            </li>
         </ul>
     </nav>
 
